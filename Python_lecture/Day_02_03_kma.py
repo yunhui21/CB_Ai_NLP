16653cc896710bf17ac625775c13342c51aba82a
yunhui21 <yunhui21@gamil.com>
2020-09-10 21:37:40 +0900

    parsing/regexp/findall

--- Python_lecture/Day_02_03_kma.py
+++ Python_lecture/Day_02_03_kma.py
@@ -27,30 +27,48 @@ for loc in location:
 
     prov = re.findall(r'<province>(.+)</province>', loc)
     city = re.findall(r'<city>(.+)</city>', loc)
-    print(prov[0], prov)
-    print(city[0])
+    # print(prov[0], prov)
+    # print(city[0])
+
+    items = re.findall(r'<mode>(.+?)</mode>.+?<tmEf>(.+?)</tmEf>.+?<wf>(.+?)</wf>', loc, re.DOTALL)
+    # print(items)
+    # print(len(items))
+
+    # for item in items:
+    #     print(item)
+
+    for mode, tmEf, wf in items:
+        print(mode, tmEf, wf)
+
 
     # data를 찾아 보세요.
-    data = re.findall(r'<data>(.+?)</data>', loc, re.DOTALL)
+    # data = re.findall(r'<data>(.+?)</data>', loc, re.DOTALL)
     # print(data)
     # print(len(data))
 
-    for datum in data:
-        # print(datum)
-        # 문제
-        # mode를 비롯한 나머지를 찾아 보세요.
-        mode = re.findall(r'<mode>(.+)</mode>', datum)
-        tmEf = re.findall(r'<tmEf>(.+)</tmEf>', datum)
-        wf   = re.findall(r'<wf>(.+)</wf>', datum)
-        tmn  = re.findall(r'<tmn>(.+)</tmn>', datum)
-        tmx  = re.findall(r'<tmx>(.+)</tmx>', datum)
-        # reli = re.findall(r'<reliability>(.+)</reliability>', datum)
-        rnSt = re.findall(r'<rnSt>(.+)</rnSt>', datum)
-        print(mode[0], tmEf[0], wf[0], tmn[0],tmx[0],rnSt[0])
-        # print(mode[0])
-        # print(tmEf[0])
-        # print(  wf[0])
-        # print( tmn[0])
-        # print( tmx[0])
-        # # print(reli[0])
-        # print( rnSt[0])
+
+
+    # for datum in data:
+    #     # print(datum)
+    #     # 문제
+    #     # mode를 비롯한 나머지를 찾아 보세요.
+    #     mode = re.findall(r'<mode>(.+)</mode>', datum)
+    #     tmEf = re.findall(r'<tmEf>(.+)</tmEf>', datum)
+    #     wf   = re.findall(r'<wf>(.+)</wf>', datum)
+    #     tmn  = re.findall(r'<tmn>(.+)</tmn>', datum)
+    #     tmx  = re.findall(r'<tmx>(.+)</tmx>', datum)
+    #     # reli = re.findall(r'<reliability>(.+)</reliability>', datum)
+    #     rnSt = re.findall(r'<rnSt>(.+)</rnSt>', datum)
+    #     # print(prov[0], city[0],mode[0], tmEf[0], wf[0], tmn[0],tmx[0],rnSt[0])
+    #
+    #     # row = '{},{},{},{},{},{},{},{}'.format(prov[0], city[0],mode[0], tmEf[0], wf[0], tmn[0],tmx[0],rnSt[0])
+    #     # print(row)
+    #     # 제주도 서귀포 A02 2020-09-13 00:00 구름많음 21 26 30
+    #     # 제주도 서귀포 A02 2020-09-13 12:00 구름많음 21 26 30
+    #
+    #     items = re.findall(r'<mode>(.+?)</mode>.+<tmEf>(.+?)</tmEf>.+<wf>(.+)</wf>', datum, re.DOTALL)
+    #     # print(items[0])   #tuple이면 [0]인덱스처럼 사용 가능
+    #     print(items[0][0], items[0][1], items[0][2])
+
+
+
