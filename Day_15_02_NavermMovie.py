fdc4c399427b187e1e74ebad8b36e28dc40c15dc
yunhui21 <yunhui21@gmail.com>
2020-09-11 12:56:51 +0900

    long_test : naver review

--- Day_15_02_NavermMovie.py
+++ Day_15_02_NavermMovie.py
@@ -10,6 +10,10 @@ import numpy as np
 import collections
 import re
 import matplotlib.pyplot as plt
+import tensorflow as tf
+
+
+
 
 # naver_ratings_test.txt, naver_ratings_test.txt
 
@@ -27,7 +31,8 @@ def get_data(file_path):
         # print(documents)
 
     f.close()
-    return documents, labels
+    small = int(len(documents)*0.3)
+    return documents[:small], labels[:small]
 
 
 def clean_str(string, TREC=False):
@@ -35,7 +40,7 @@ def clean_str(string, TREC=False):
     Tokenization/string cleaning for all datasets except for SST.
     Every dataset is lower cased except for TREC
     """
-    string = re.sub(r"[^가-힣A-Za-z0-9(),!?\'\`]", " ", string)
+    string = re.sub(r"[^가-힣A-Za-z0-9(),!?\'\'] " , " ", string)
     string = re.sub(r"\'s", " \'s",   string)
     string = re.sub(r"\'ve", " \'ve", string)
     string = re.sub(r"n\'t", " n\'t", string)
@@ -65,9 +70,56 @@ def make_same_length(documents, max_size):
 
 
 x_train, y_train = get_data('data/naver_ratings_train.txt')
-# x_test, y_test = get_data('data/naver_ratings_test.txt')
+x_test, y_test = get_data('data/naver_ratings_test.txt')
 
-x_train = make_same_length(x_train, max_size=25)
+y_train = np.reshape(y_train, [-1,1])
+y_test = np.reshape(y_test, [-1,1])
+
+# x_train = make_same_length(x_train, max_size=25)
 # x_test  = make_same_length(x_test, max_size=25)
 
+vocab_size = 2000
+tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=vocab_size)
+tokenizer.fit_on_texts(x_train)
 
+print(tokenizer.index_word)
+
+x_train = tokenizer.texts_to_sequences(x_train)
+x_test = tokenizer.texts_to_sequences(x_test)
+print(x_train[0])
+
+x_train = tf.keras.preprocessing.sequence.pad_sequences(x_train, padding='post', maxlen=25)
+x_test = tf.keras.preprocessing.sequence.pad_sequences(x_test, padding='post', maxlen=25)
+
+print(x_train[0])
+
+print(x_train.shape, y_train.shape)     # (150000, 25) (150000, 1) lstm 3차원
+# tensor
+# ensor = (5,6)
+model = tf.keras.Sequential([
+            tf.keras.layers.Embedding(vocab_size, 100),
+            tf.keras.layers.LSTM(50),
+            tf.keras.layers.Dense(1, activation=tf.keras.activations.sigmoid)
+        ])
+
+model.compile(optimizer=tf.keras.optimizers.Adam(0.01),
+              loss=tf.keras.losses.binary_crossentropy)
+model.fit(x_train, y_train, epochs=5, verbose=2, batch_size=128)
+print('acc:', model.evaluate(x_test, y_test))
+
+
+# 여러분의 리뷰가 긍정인지 부정인지 단계별로 알려주세요.
+#
+
+new_review = '지금껏 본것중에서 가장 감동적인 영화...'
+tokens = clean_str(new_review).split()
+x_review = tokenizer.texts_to_sequences([tokens])       #1차원 -> 2차원
+x_review = x_review[0]                                  #2차원 -> 1차원
+
+for i in range(len(x_review)):
+    words = [x_review[:i+1]]
+    words = tf.keras.preprocessing.sequence.pad_sequences(words, padding='post', maxlen=25)
+
+    # print(x_review[:i+1])
+    print(tokens[:i+1])
+    print(model.predict(words, verbose=0))
