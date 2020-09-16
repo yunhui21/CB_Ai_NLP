ab53a64e2f61e87a45302d503eab7a0664f85892
yunhui21 <yunhui21@gmail.com>
2020-09-16 10:53:23 +0900

    17flowers_make_data

--- Day_18_01_17flowers.py
+++ Day_18_01_17flowers.py
@@ -1,107 +1,53 @@
-# # Day_18_01_17flowers.py
-import Image
-import os, sys
-
-# def resizeImage(infile, output_dir="data/17flowers_origin", size=(1024,768)):
-#      outfile = os.path.splitext(infile)[0]+"_resized"
-#      extension = os.path.splitext(infile)[1]
-#
-#      if (cmp(extension, ".jpg")):
-#         return
-#
-#      if infile != outfile:
-#         try :
-#             im = Image.open(infile)
-#             im.thumbnail(size, Image.ANTIALIAS)
-#             im.save(output_dir+outfile+extension,"JPEG")
-#         except IOError:
-#             print "cannot reduce image for ", infile
-#
-#
-# if __name__=="__main__":
-#     output_dir = "resized"
-#     dir = os.getcwd()
-#
-#     if not os.path.exists(os.path.join(dir,output_dir)):
-#         os.mkdir(output_dir)
-#
-#     for file in os.listdir(dir):
-#         resizeImage(file,output_dir)
-
-# import Image
-# import os
-# import sys
-#
-# directory = sys.argv[1]
-#
-# for file_name in os.listdir(directory):
-#   print("Processing %s" % file_name)
-#   image = Image.open(os.path.join(directory, file_name))
-#
-#   # x,y = image.size
-#   new_dimensions = (200, 200)
-#   output = image.resize(new_dimensions, Image.ANTIALIAS)
-#
-#   output_file_name = os.path.join(directory, "small_" + file_name)
-#   output.save(output_file_name, "JPEG", quality = 95)
-#
-# print("All done")
-
-import os
-from PIL import Image
-
-resize_method = Image.ANTIALIAS
-# Image.NEAREST)  # use nearest neighbour
-# Image.BILINEAR) # linear interpolation in a 2x2 environment
-# Image.BICUBIC) # cubic spline interpolation in a 4x4 environment
-# Image.ANTIALIAS) # best down-sizing filter
-# import os
-# from PIL import Image
-# resize_method = Image.ANTIALIAS
-#
-# max_height = 1200
-# max_width = 1200
-# extensions = ['JPG']
-#
-# path = os.path.abspath(".")
-#
-#
-# def adjusted_size(width, height):
-#     if width > max_width or height > max_height:
-#         if width > height:
-#             return max_width, int(max_width * height / width)
-#         else:
-#             return int(max_height * width / height), max_height
-#     else:
-#         return width, height
-#
-#
-# if __name__ == "__main__":
-#     for f in os.listdir(path):
-#         if os.path.isfile(os.path.join(path, f)):
-#             f_text, f_ext = os.path.splitext(f)
-#             f_ext = f_ext[1:].upper()
-#             if f_ext in extensions:
-#                 print
-#                 f
-#                 image = Image.open(os.path.join(path, f))
-#                 width, height = image.size
-#                 image = image.resize(adjusted_size(width, height))
-#                 image.save(os.path.join(path, f))
-
-import cv2
-import glob
+# Day_18_01_17flowers.py
+# 문제
+# origin 폴더에 있는 이미지를 224호 축소해서 224 폴더로 복사하세요.
+# 이름을 17flowers_origin
+import tensorflow as tf
+import numpy as np
+from PIL import Image # pillow
+# import PIL 자동완성기능이 안된다.
 import os
 
-inputFolder = 'data/17flowers_orign'
-folderlen = len(inputFolder)
-os.mkdir('17flowers_224')
-i = 0
-for img in glob.glob(inputFolder + "/*.jpg")
-    image = cv2.imread(img)
-    imgResized = cv2.resize(image, (224, 224))
-    cv2.imwrite("17flowers_224"+ img[folderlen:], imgResized)
-    i += 1
-    cv2.imahow('image', imgResized)
-    cv2.waitKey(30)
-cv2.destroyAllWindows()
+# 문제
+# 꽃 폴더로부터 파일을 읽어서 x, y 데이터를 만드세요.
+# y: 80개 간격이 나누어져 있습니다. (0-16)
+# x: 4차원 배열
+
+def make_flowers_224(src_folder, dst_folder, img_size=[224, 224]):
+    for filename in os.listdir(src_folder):
+        print(filename)
+
+        items = filename.split('.')
+        if items[-1] != 'jpg':
+            continue
+
+        # file_path = src_folder + '/' + filename
+        # print(file_path)
+        file_path = os.path.join(src_folder, filename)
+        print(file_path)
+        image1 = Image.open(file_path)
+        image2 = image1.resize(img_size)
+        image2.save(os.path.join(dst_folder, filename))
+
+
+def make_flowers_xy(dir_name):
+    x , y = [], []
+    for filename in os.listdir(dir_name):
+        items = filename.split('.')
+        if items[-1] != 'jpg':
+            continue
+
+        idx = int(items[0].split('_')[1])
+        print(idx, (idx-1) // 80)
+        y.append((idx - 1)// 80)
+
+        file_path = os.path.join(dir_name, filename)
+        img = Image.open(file_path)
+        # print(type(img))
+        # print(type(np.array(img)), np.array(img).shape)
+
+    return np.array(x), np.array(y), len(set(y))
+
+# make_flowers_224('data/17flowers_origin', 'data/17flowers_224', img_size=[56, 56])
+x, y, n_classes = make_flowers_xy('17flowers_224')
+
