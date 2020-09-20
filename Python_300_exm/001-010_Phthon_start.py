4d2cdff2493102ec6792c126eae0f92bfb291b26
Yunhui Song <63230214+yunhui21@users.noreply.github.com>
2020-09-20 17:34:15 +0900

    python_300_start

---
+++ Python_300_exm/001-010_Phthon_start.py
@@ -0,0 +1,30 @@
+# 001-010_Phthon_start.py
+# 001
+print('001:','hello world')
+
+# 002
+print("002: Mary's cosmetics")
+
+# 003
+print('003:', '신씨가 소리질렀다. "도둑이야".')
+
+# 004
+print('004: "C:\windows')
+
+# 005
+print('005: "안녕하세요. \n 만나서 \t\t 반갑습니다."')
+
+# 006
+print('006:', "오늘은", "일요일")
+
+# 007
+print('007: naver;kakao;sk;samsung')
+
+# 008
+print('008: naver/kakao/sk/samsung')
+
+# 009
+print('009: first', end='');print('second')
+
+# 010
+print('010: 5/3 = ', 5/3)
