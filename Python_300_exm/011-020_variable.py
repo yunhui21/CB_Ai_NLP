7d714eb8d3cf0bad760fe9deb477c19dd7d2df8d
Yunhui Song <63230214+yunhui21@users.noreply.github.com>
2020-09-20 17:34:51 +0900

    python_300_variable

---
+++ Python_300_exm/011-020_variable.py
@@ -0,0 +1,54 @@
+# 011-020_variable.py
+
+# 011
+samsung = 50000
+print('011: 삼성전자주식 10주 =', samsung*10)
+
+# 012
+시가총액 = '298조'
+현재가 = '50000원'
+PER = 15.79
+print('012:', '시가총액 =',시가총액, '현재가 =',현재가, 'PER =',PER)
+
+# 013
+s = 'Hello'
+t = 'python'
+print('013:', s+'!', t)
+
+# 014
+print('014:', 2+2*3)
+
+# 015
+a = '128'
+print('015:', type(a))
+
+# 016
+num_str = '720'
+num_int = int(num_str)
+print('016:', type(num_int))
+
+# 017
+num = 100
+result = str(num)
+print('017:', type(result))
+
+# 018
+str = "15.79"
+result = float(str)
+print('018:', type(result))
+
+# 019
+year = '2020'
+result = int(year)
+print('018:', result-3, result-2, result-1)
+
+# 020
+intell = 48584
+month = 36
+air_price = intell*month
+print('020: 에어컨가격 =', air_price)
+
+
+
+
+print
