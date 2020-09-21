950a9197d4e95b21c348cb7089b14fb8673864a7
yunhui21 <yunhui21@gamil.com>
2020-09-21 22:19:31 +0900

    string/format()/replace()/strip()/slicing

--- Python_300_exm/031-040_str연산.py
+++ Python_300_exm/031-040_str연산.py
@@ -22,7 +22,26 @@ age1 = 10
 name2 = '이철희'
 age2 = 12
 
-print('035:', '이름: ', name1, '나이:', age1)
-print('035:', '이름: ', name2, '나이:', age2)
+print('035:', '이름:', name1, '나이:', age1)
+print('035:', '이름:', name2, '나이:', age2)
 
 # 036
+print('036:', '이름: {} 나이: {}'.format(name1, age1))
+print('036:', '이름: {} 나이: {}'.format(name2, age2))
+
+# 037
+print('037:', f'이름: {name1} 나이: {age1}')
+print('037:', f'이름: {name2} 나이: {age2}')
+
+# 038
+상장주식주 = "5,969,782,550"
+int = int(상장주식주.replace(',',''))
+print('038:', int, type(int))
+
+# 039
+분기 = '2020/03(E) (IFRS연결)'
+print('039:', 분기[:7])
+
+# 040
+data = '     삼성전자      '
+print('040:', data.strip())
