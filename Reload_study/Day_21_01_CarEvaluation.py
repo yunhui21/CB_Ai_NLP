bd0dcd2738e1ce2d7a88d61def719bf51b536133
yunhui21 <yunhui21@gmail.com>
2020-09-22 10:00:50 +0900

    study/get_data_making

--- Reload_study/Day_21_01_CarEvaluation.py
+++ Reload_study/Day_21_01_CarEvaluation.py
@@ -7,9 +7,12 @@ from sklearn import preprocessing, model_selection
 
 def get_car_dense():
 
-    cars = pd.read_csv('data/carse.data', 'r', header=None)
+    cars = pd.read_csv('../data/car.data', 'r', header=None, names=['buying','maint','doors','persons','lug_boot','safety', 'eval'])
     print(cars)
     lb = preprocessing.label_binarize()
+    buying = lb.fit_transform(cars.buying)
+    maint  = lb.fit_transform(cars.maint)
+    doors  = lb.fit_transform(cars.doors)
 
 
 
