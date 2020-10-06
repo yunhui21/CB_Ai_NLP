bce62949311f2a5607e1e396d9f56ea50e120123
yunhui21 <system290!>
2020-10-06 23:28:38 +0900

    for/==/!=/></>=/<=

---
+++ AlgorithTrading/Chapter04_for.py
@@ -0,0 +1,25 @@
+# 1)Boolean
+a = True
+print(type(a))
+b = False
+print(type(b))
+# True False은 파이썬의 예약어로 대문자로 표기해야함.
+
+'''
+연산자     연산자 의미
+==        같다
+!=        다르다
+>         크다
+<         작다
+>=        크거나 작다
+<=        작거나 같다
+'''
+print(3==3)
+print(3!=3)
+print(3<3)
+print(3>3)
+print(3<=3)
+print(3>=3)
+
+mystock = 'naver'
+print(mystock == 'naver')
