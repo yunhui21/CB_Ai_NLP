070844939de2a1d6e88150fd770d3573c3cd1b5a
yunhui21 <system290!>
2020-10-13 17:41:30 +0900

    function/module

--- AlgorithTrading/Chapter05_function&module.py
+++ AlgorithTrading/Chapter05_function&module.py
@@ -23,17 +23,58 @@ def print_ntimes(n):
 # print_ntimes(3)
 
 # 5)-2 반환값이 있는 함수
+# 5)-2-1 함수 호출 과정 이해하기
 
 def cal_upper(price):
     pass
 
-def call_upper(price):
+def cal_upper(price):
     increment = price * 0.3
     upper_price = price + increment
     return upper_price
+# cal_upper(10000)
+print(cal_upper(10000))
 
-call_upper(10000)
+def cal_lower(price):
+    decrement = price * 0.3
+    lower_price = price - decrement
+    return lower_price
 
+print(cal_lower(1000))
 
-# 5)-2-1 함수 호출 과정 이해하기
+# 5)-2-2 두 개의 값 반환하기
+def cal_upper_lower(price):
+    offset = price * 0.3
+    upper = price + offset
+    lower = price - offset
+    return (upper, lower)
 
+(upper, lower) = cal_upper_lower(10000)
+print(upper)
+print(lower)
+
+# 5)-3 모듈
+# 모듈 : 프로그램이나 하드웨어 기능의 단위'라는 의미를 가짐.
+# 모듈은 함수보다 상위 개념으로 함수와 마찬가지로 코드의 재사용을 위해 사용합니다.
+
+# 5)-3-1 모듈 만들기
+import stock
+
+print(stock.author)
+upper_price = stock.cal_upper(10000)
+print(upper_price)
+lower_price = stock.cal_lower(10000)
+print(lower_price)
+
+if __name__ == '__main__':
+    print(cal_upper(10000))
+    print(cal_lower(10000))
+    print(__name__)
+
+# 5)-3-2 파이썬에서 시간 다루기
+# 시계열 : 일정 시간 간격으로 배치된 데이터의 수열
+# 시간과 날짜를 다루는 모듈 : time , datetime
+
+import time
+print(time.time())
+print(time.ctime())
